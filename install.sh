04fe1d00037d91f173624df6a901a47c7669617d
Nikita Gusakov <dev@nkt.me>
2014-10-17 22:36:31 +0400

    Small improvements

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 HOSTNAME="osx-nkt"
 COMPNAME=$(echo $HOSTNAME | tr '-' '.')
-
-echo 'Creating projects directory...'
-mkdir "$HOME/Projects"
+PUBKEY="$HOME/.ssh/id_rsa.pub"
 
 if [[ `uname` == 'Darwin' ]]; then
     which -s brew
@@ -13,22 +11,26 @@ if [[ `uname` == 'Darwin' ]]; then
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/go/install)"
     fi
 
+    mkdir -p "$HOME/Projects"
+
     echo 'Replacing default paths file...'
     sudo cp -f etc/osx/paths /etc/paths
 
-    scutil --set HostName "$HOSTNAME"
-    scutil --set ComputerName "$COMPNAME"
-    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPNAME"
+    if [[ $(scutil --get HostName) != "$HOSTNAME" ]]; then
+        scutil --set HostName "$HOSTNAME"
+        scutil --set ComputerName "$COMPNAME"
+        sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPNAME"
+    fi
 fi
+
+echo 'Copying dotfiles...'
+for dotfile in files/.*; do
+    if [[ -f $dotfile ]]; then
+        cp -f $dotfile "$HOME/"
+    fi
+done
 
 if [[ ! -f "$PUBKEY" ]]; then
     echo 'Generating new SSH key..'
-    ssh-keygen -t rsa
+    ssh-keygen -t rsa -f "$PUBKEY"
 fi
-
-echo 'Symlink dotfiles...'
-for dotfile in files/.*; do
-    if [[ -f $dotfile ]]; then
-        ln -s $dotfile "$HOME/"
-    fi
-done
